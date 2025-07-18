b749ba763ec57838c9816d50636476feebf35fa6
LastHope <nikita_pishkov@mail.ru>
2025-07-19 00:55:18 +0400

    Выполнил задание к лекции 7

--- src/main/java/com/example/demo/repository/VisitorRepository.java
+++ src/main/java/com/example/demo/repository/VisitorRepository.java
@@ -1,29 +1,7 @@
 package com.example.demo.repository;
 
 import com.example.demo.model.Visitor;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.function.Predicate;
-
-@Repository
-public class VisitorRepository {
-    private final List<Visitor> visitors = new ArrayList<>();
-
-    public void save(Visitor visitor) {
-        visitors.add(visitor);
-    }
-
-    public void remove(Visitor visitor) {
-        visitors.remove(visitor);
-    }
-
-    public void removeIf(Predicate<Visitor> predicate) {
-        visitors.removeIf(predicate);
-    }
-
-    public List<Visitor> findAll() {
-        return visitors;
-    }
+public interface VisitorRepository extends JpaRepository<Visitor, Long> {
 }
